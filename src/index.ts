370912c515dfd73618d0fcf919493a385d210e6a
resumi30 <resumi30minutes@gmail.com>
2026-04-29 20:53:41 +0900

    第2章まで 基本構文

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+
+const fizzBuzz = () => {
+    let n: number = 1;
+    while (n < 100) {
+        if (n % 3 === 0 && n % 5 === 0) {
+            console.log('FizzBuzz')
+        } else if (n % 3 === 0) {
+            console.log('Fizz')
+        } else if (n % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(n);
+        }
+
+        n++;
+    }
+}
+
+
+fizzBuzz();
